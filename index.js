b587778d82f77e165ca2890144ff5bb8b528ca44
nihal0567 <alexnillc@gmail.com>
2025-12-09 14:14:56 +0600

    mada a api to find product collection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config()
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 9000;
@@ -9,39 +11,51 @@ const port = 9000;
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-  res.send('app is running')
-})
-app.listen(port, ()=>{
-  console.log(`app running on this port ${port}`);
-})
-
 const uri = `mongodb+srv://${process.env.VITE_DB_USER}:${process.env.VITE_DB_PASS}@cluster0.ue9fgze.mongodb.net/?appName=Cluster0`;
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
+console.log(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-//   const db = client.db("myDb")
-//   const dataCollection = db
+app.get('/', (req, res)=>{
+  res.send('app is running')
+})  
 
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+async function run() {
+
+  try {
+    await client.connect();
+
+    const db = client.db('myDb')
+    const dataCollection =  db.collection('products-collection')
+
+    app.get('/products-collection', async(req, res)=>{
+      const result =await dataCollection.find().toArray()
+      res.send(result)
+    })
+   
+    // app.post('/products-collection', async(req, res)=>{
+    //     const data = req.body
+    //     const result = dataCollection.insertOne()
+    // })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, ()=>{
+  console.log(`app running on this port ${port}`);
+})  
 
 
 
